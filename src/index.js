dd3bb80c93acca658584fffc33bbac7dd57dd041
Sergey Volkov <s.volkov@netris.ru>
2019-05-14 14:37:20 +0300

    Send events to server

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const DEFAULT_FPF = 6;
 const Start = function() {
   const element = document.getElementById('videoTagId');
   let converter;
+  let streamInfo;
 
   const websocket = new WebSocket(wsUrl);
   websocket.binaryType = 'arraybuffer';
@@ -27,6 +28,7 @@ const Start = function() {
           converter.appendRawData(new Uint8Array([]));
           converter.pause();
         }
+        streamInfo = data;
         converter = new VideoConverter(element, data.frameRate, DEFAULT_FPF);
         converter.play();
         break;
@@ -38,6 +40,9 @@ const Start = function() {
       }
     }
   }, false);
+
+  const inputWrapperId = 'inputWrap';
+  const controlsWrapperId = 'controlsWrap';
   const btn = document.getElementById('start');
   btn.innerText = 'Stop';
   btn.onclick = function() {
@@ -45,8 +50,115 @@ const Start = function() {
     converter.pause();
     btn.innerText = 'Start';
     btn.onclick = Start;
+    const textWrap = document.getElementById(inputWrapperId);
+    if (textWrap) {
+      textWrap.parentElement.removeChild(textWrap);
+    }
+  };
+
+  const textWrap = document.createElement('div');
+  textWrap.id = inputWrapperId;
+  const input = document.createElement('input');
+  const sendButton = document.createElement('button');
+  sendButton.innerText = 'Send';
+  textWrap.appendChild(input);
+  textWrap.appendChild(sendButton);
+  document.getElementById(controlsWrapperId).appendChild(textWrap);
+  sendButton.onclick = function() {
+    if (websocket && websocket.readyState === websocket.OPEN) {
+      const type = CONST.MESSAGE_TYPE_EVENT;
+      const text = input.value;
+      const eventType = CONST.EVENT_TYPE.TEXT;
+      websocket.send(JSON.stringify({type: type, text: text, eventType: eventType}));
+    }
+  };
+
+  document.body.oncontextmenu = function(e) {
+    e.preventDefault();
+    return false;
+  };
+
+  let down = 0;
+
+  function onMouseEvent(e) {
+    if (e.target === element && websocket && websocket.readyState === websocket.OPEN) {
+      const event = buildMotionEvent(e);
+      if (event) {
+        websocket.send(JSON.stringify(event));
+      }
+      e.preventDefault();
+      e.stopPropagation();
+      return false;
+    }
   }
+
+  const EVENT_ACTION_MAP = {
+    'mousedown': CONST.MOTION_EVENT.ACTION_DOWN,
+    'mousemove': CONST.MOTION_EVENT.ACTION_MOVE,
+    'mouseup': CONST.MOTION_EVENT.ACTION_UP
+  };
+
+  const BUTTONS_MAP = {
+    0: 17, // ?? BUTTON_PRIMARY
+    1: CONST.MOTION_EVENT.BUTTON_TERTIARY,
+    2: 26  // ?? BUTTON_SECONDARY
+  };
+
+  function buildMotionEvent(e) {
+    const action = EVENT_ACTION_MAP[e.type];
+    if (typeof action === 'undefined') {
+      return null;
+    }
+    let {clientWidth, clientHeight} = element;
+    const width = streamInfo.width;
+    const height = streamInfo.height;
+    let touchX = (e.clientX - e.target.offsetLeft);
+    let touchY = (e.clientY - e.target.offsetTop);
+    const eps = 1e5;
+    const ratio = width / height;
+    if (Math.round(eps * ratio) > Math.round(eps * clientWidth / clientHeight)) {
+      const realHeight = Math.ceil(clientWidth / ratio);
+      const top = (clientHeight - realHeight) / 2;
+      if (touchY < top || touchY > top + realHeight) {
+        return null;
+      }
+      touchY -= top;
+      clientHeight = realHeight;
+    }
+    const x = touchX * width / clientWidth;
+    const y = touchY * height / clientHeight;
+
+    const type = CONST.MESSAGE_TYPE_EVENT;
+    const eventType = CONST.EVENT_TYPE.MOUSE;
+    return {
+      type: type,
+      eventType: eventType,
+      position: {
+        x: x,
+        y: y,
+        width: width,
+        height: height
+      },
+      buttons: BUTTONS_MAP[e.button],
+      action: action
+    };
+  }
+
+  document.body.onmousedown = function(e) {
+    down++;
+    onMouseEvent(e);
+  };
+  document.body.onmouseup = function(e) {
+    down--;
+    onMouseEvent(e);
+  };
+  document.body.onmousemove = function(e) {
+    if (down > 0) {
+      onMouseEvent(e);
+    }
+  };
 };
+
 
 window.onload = function() {
   const btn = document.getElementById('start');
