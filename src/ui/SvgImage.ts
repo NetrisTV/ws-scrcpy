b6d6fecbde942ab362c2b3eb844b5eae983cb568
Sergey Volkov <s.volkov@netris.ru>
2020-08-04 18:53:18 +0300

    Move *.svg under images/

--- src/ui/SvgImage.ts
+++ src/ui/SvgImage.ts
@@ -1,12 +1,12 @@
-import KeyboardSVG from '../../svg/skin-light/ic_keyboard_678_48dp.svg';
-import MoreSVG from '../../svg/skin-light/ic_more_horiz_678_48dp.svg';
-import CameraSVG from '../../svg/skin-light/ic_photo_camera_678_48dp.svg';
-import PowerSVG from '../../svg/skin-light/ic_power_settings_new_678_48px.svg';
-import VolumeDownSVG from '../../svg/skin-light/ic_volume_down_678_48px.svg';
-import VolumeUpSVG from '../../svg/skin-light/ic_volume_up_678_48px.svg';
-import BackSVG from '../../svg/skin-light/System_Back_678.svg';
-import HomeSVG from '../../svg/skin-light/System_Home_678.svg';
-import OverviewSVG from '../../svg/skin-light/System_Overview_678.svg';
+import KeyboardSVG from '../../images/skin-light/ic_keyboard_678_48dp.svg';
+import MoreSVG from '../../images/skin-light/ic_more_horiz_678_48dp.svg';
+import CameraSVG from '../../images/skin-light/ic_photo_camera_678_48dp.svg';
+import PowerSVG from '../../images/skin-light/ic_power_settings_new_678_48px.svg';
+import VolumeDownSVG from '../../images/skin-light/ic_volume_down_678_48px.svg';
+import VolumeUpSVG from '../../images/skin-light/ic_volume_up_678_48px.svg';
+import BackSVG from '../../images/skin-light/System_Back_678.svg';
+import HomeSVG from '../../images/skin-light/System_Home_678.svg';
+import OverviewSVG from '../../images/skin-light/System_Overview_678.svg';
 
 enum Icon {
     BACK,
