a454fca4647f11ca82b8c7a9b1b60e04cb69664a
PetrasRec <petvic@uber.com>
2023-11-09 20:26:45 +0000

    feat(scrcpy): add scrcpy_ prefix to all metrics

--- src/server/services/PromMetrics.ts
+++ src/server/services/PromMetrics.ts
@@ -5,31 +5,31 @@ const labels = ['player_name', 'user_email'];
 const playerNames = ['Broadway.js', 'H264 Converter', 'Tiny H264', 'WebCodecs'];
 
 const decodedFramesGauge = new promClient.Gauge({
-    name: 'decoded_frames',
+    name: 'scrcpy_decoded_frames',
     help: 'Number of decoded frames per second',
     labelNames: labels,
 });
 
 const droppedFramesGauge = new promClient.Gauge({
-    name: 'dropped_frames',
+    name: 'scrcpy_dropped_frames',
     help: 'Number of dropped frame per second',
     labelNames: labels,
 });
 
 const inputFramesGauge = new promClient.Gauge({
-    name: 'input_frames',
+    name: 'scrcpy_input_frames',
     help: 'Number of input frames per second',
     labelNames: labels,
 });
 
 const inputBytesGauge = new promClient.Gauge({
-    name: 'input_bytes',
+    name: 'scrcpy_input_bytes',
     help: 'Number of input bytes per second',
     labelNames: labels,
 });
 
 const webSocketConnections = new promClient.Gauge({
-    name: 'ws_active_connections',
+    name: 'scrcpy_ws_active_connections',
     help: 'Number of active WebSocket connections',
     labelNames: ['user_email'],
 });
