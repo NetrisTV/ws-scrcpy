2a4aa3ecaa8c815705df4ad750da5cfeeeec762a
Sergey Volkov <drauggres@gmail.com>
2025-01-05 21:05:46 +0300

    feat: add Android 14 and 15 support

--- src/common/Constants.ts
+++ src/common/Constants.ts
@@ -1,6 +1,6 @@
 export const SERVER_PACKAGE = 'com.genymobile.scrcpy.Server';
 export const SERVER_PORT = 8886;
-export const SERVER_VERSION = '1.19-ws5';
+export const SERVER_VERSION = '1.19-ws6';
 
 export const SERVER_TYPE = 'web';
 
